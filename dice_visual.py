a1c08b14bca858f4c8c8103a81e6629e5757b15b
Roman <roman.matrosov.frontend@yandex.ru>
2022-09-11 23:24:10 +0300

    Two not equal dices (D6 and D10)

--- dice_visual.py
+++ dice_visual.py
@@ -5,11 +5,11 @@ from die import Die
 
 # Создание двух кубиков D6
 die1 = Die()
-die2 = Die()
+die2 = Die(10)
 
 # Моделирование серии бросков с сохранением результата в списке
 results = []
-for roll_num in range(1000):
+for roll_num in range(50000):
     result = die1.roll() + die2.roll()
     results.append(result)
 
@@ -25,6 +25,6 @@ data = Bar(x=x_values, y=frequencies)
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of result'}
-my_layout = Layout(title='Results of rolling two dices D6 1000 times',
+my_layout = Layout(title='Results of rolling a D6 and a D10 50000 times',
                   xaxis=x_axis_config,yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
